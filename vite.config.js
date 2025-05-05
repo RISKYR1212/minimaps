65d48edfe1acec6defcd9e0d8cf18e281f6368d0
RISKYR1212 <rizkyrispaldi0@gmail.com>
2025-05-06 05:53:07 +0700

    sekali lagi revisi

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    historyApiFallback: true,
+    host: "localhost",
+    // host: "127.0.0.1",
+    port: 5173,
   },
 })
