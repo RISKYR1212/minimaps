57b18ea1d373433b9d732870d12b39fe3508b4de
RISKYR1212 <rizkyrispaldi0@gmail.com>
2025-08-21 11:20:14 +0700

    coba lagi yaa

--- vite.config.js
+++ vite.config.js
@@ -3,43 +3,36 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-plugins: [
+  plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: 'REPORT FOC',
-        short_name: 'RFOC',
-        theme_color: '#ffffff',
+        name: "REPORT FOC",
+        short_name: "RFOC",
+        theme_color: "#ffffff",
         icons: [
-            {
-                src: 'pwa-64x64.png',
-                sizes: '64x64',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any'
-            },
-            {
-                src: 'maskable-icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable'
-            }
+          { src: "pwa-64x64.png", sizes: "64x64", type: "image/png" },
+          { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
-      }, 
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000, 
+      },
     }),
   ],
   resolve: {
@@ -49,6 +42,6 @@ plugins: [
   },
   server: {
     host: true,
-    allowedHosts: [".ngrok-free.app"]
+    allowedHosts: [".ngrok-free.app"],
   },
-})
+});
